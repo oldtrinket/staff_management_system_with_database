a5aa74c151cdd4529d5ab989ef9a9f1bc5513eef
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-10-30 23:07:46 +1100

    Add files via upload

---
+++ delete_limited.php
@@ -0,0 +1,19 @@
+<?php
+if ( isset($_GET['staffID']) ) {
+    $staffID = $_GET['staffID'];
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "fastfood";
+
+    $connection = new mysqli($servername, $username, $password, $database);
+
+    $sql = "DELETE FROM staff WHERE staffID = $staffID";
+    $connection->query($sql);
+    
+}
+
+header("Location: /henry_ff/index4.php");
+exit;
+?>
