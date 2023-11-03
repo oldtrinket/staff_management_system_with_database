5a7a3ad4474b36fca1d816421fad4d087f2d20a2
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-11-03 20:51:39 +1100

    Update login.php

--- login.php
+++ login.php
@@ -1,37 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-    <style>
-        body {
-            text-align: center;
-        }
-        h1 {
-            margin-top: 10px;
-        }
-        .login-box {
-            width: 30%;
-            margin: auto;
-        }
-        input[type=email], input[type=password] {
-            width: 100%; 
-            padding: 12px 20px; 
-            margin: 8px 0;  
-            display: inline-block; 
-            border: 1px solid #ccc;  
-            box-sizing: border-box;  
-        }
-        button {
-            background-color: blue;
-            color: white;
-            width: 100%; 
-            padding: 14px 20px;
-            font-size: 18px; 
-        }
-        .header {
-            text-align: center;
-            margin-bottom: 20px;
-        }
+
   
            
         
