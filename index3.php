ce9b84f3d253aadfd50b310700e9ae829bfebfb6
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-11-08 19:21:35 +1100

    Update index3.php

--- index3.php
+++ index3.php
@@ -1,4 +1,4 @@
-<?php // sdfsd sdfsd sdfasd sdfasa 
+<?php // sdfsd sdfsd sdfasd 
 // At the top of index.php
 $updateSuccess = false;
 if (isset($_GET['updateSuccess']) && $_GET['updateSuccess'] == 1) {
