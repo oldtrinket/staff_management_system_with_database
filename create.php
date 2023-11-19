c58119aa61f85fa9b762bfa2bb284100195b7dbe
Henry Gervacio <114787643+oldtrinket@users.noreply.github.com>
2023-11-19 22:56:30 +1100

    Update create.php

--- create.php
+++ create.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = 
+$database  
 
 //create connection
 $connection = new mysqli($servername, $username, $password, $database);
